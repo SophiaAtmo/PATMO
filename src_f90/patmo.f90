e4714c642551e75e6cc5980ad3d41e307729057b
Yuanzhe LI <59906908+PatrickYLi@users.noreply.github.com>
2025-10-17 14:05:18 +0900

    Add files via upload

--- src_f90/patmo.f90
+++ src_f90/patmo.f90
@@ -7,6 +7,8 @@ contains
     use patmo_photo
     use patmo_parameters
     use patmo_utils
+
+#IFPATMO_usePhotochemistry
     !load photo metrics (i.e. binning)
     call loadPhotoMetric("xsecs/photoMetric.dat")
     !load photo cross-sections
@@ -14,6 +16,7 @@ contains
 
     !init default photoflux
     photoFlux(:) = 0d0
+#ENDIFPATMO
 
     !init cumulative flux for entropy production
     ! (i.e. integrated with time)
@@ -29,7 +32,7 @@ contains
 
   !**************
   !run model for a time-step
-  subroutine patmo_run(dt,convergence)
+  subroutine patmo_run(dt)
     !use dvode_f90_m
     use patmo_parameters
     use patmo_commons
@@ -42,21 +45,18 @@ contains
     use patmo_utils
     implicit none
     real*8,intent(in)::dt
-    real*8,intent(out)::convergence
     real*8::atol(neqAll),rtol(neqAll)
     real*8::tstart,tend,n(neqAll)
     real*8::sumxn(photoBinsNumber),m(speciesNumber)
     !type(VODE_OPTS)::OPTIONS
     integer::istate,itask,i,j
+
     integer,parameter::meth=2
     integer,parameter::lwm=2*neqAll**2 + 2*neqAll &
          + (neqAll**2+10*neqAll)/2
     integer::itol,iopt,mf,lrw,liw
     integer::iwork(20+9*neqAll+LWM),neq(1)
     real*8::rwork(20+neqAll*6+3*neqAll+lwm)
-    integer :: first = 1
-    real*8  :: total_species = 0.0
-    real*8  :: total_species_old = 0.0
 
     lrw = size(rwork)
     liw = size(iwork)
@@ -93,6 +93,7 @@ contains
     tstart = 0d0
     tend = dt
 
+#IFPATMO_use_opacity
     !upper layer opacity is zero
     tauAll(:,cellsNumber) = 0d0
     !loop on cells
@@ -104,6 +105,7 @@ contains
        end do
        tauAll(:,j) = tauAll(:,j+1) + gridSpace(j) * sumxn(:)
     end do
+#ENDIFPATMO
 
     !unroll chemistry
     do i=1,speciesNumber
@@ -114,32 +116,30 @@ contains
          = TgasAll(:)
 
     !compute rates
-    if (mod(first,20) .eq. 0) then
-            call computeRates(TgasAll(:))
-            call computePhotoRates(tauAll(:,:))
-            call computeReverseRates(TgasAll(:))
-            print *, 'Current convergence:', convergence
-    endif
-    first = first+1
-    
+    call computeRates(TgasAll(:))
+    call computePhotoRates(tauAll(:,:))
+    call computeReverseRates(TgasAll(:))
+#IFPATMO_useHescape 
+    call computeHescape() 
+#ENDIFPATMO
     !compute tot density
-    !ntotAll(:) = sum(nall(:,1:chemSpeciesNumber),2)
+    !ntotAll(:) = sum(nall(:,1:chemSpeciesNumber),2) !Trieu commented
     ntotAll(:) = 0.0d0
     do j = 1, chemSpeciesNumber
       if (j /= patmo_idx_M) then
        ntotAll(:) = ntotAll(:) + nAll(:, j)
-       total_species = total_species + sum(nAll(cellsNumber,1:chemSpeciesNumber))
-       convergence = (total_species - total_species_old)*100/total_species
-       total_species_old = total_species
-       if (abs(convergence) < 1e-10) print *, 'Convergence/steady state reached'
       end if
-    end do
+    end do                                           !Trieu commented
 
     !compute mean molecular mass of the whole atmosphere
     ! (averaged between layers)
     m(:) = getSpeciesMass()
     meanMolecularMass = 0d0
     do i=1,cellsNumber
+    !   meanMolecularMass = meanMolecularMass &       !Trieu commented
+    !        + sum(m(1:chemSpeciesNumber) &
+    !        * nAll(i,1:chemSpeciesNumber)) &
+    !        / ntotAll(i) / cellsNumber
        do j = 1, chemSpeciesNumber
          if (j /= patmo_idx_M) then
            meanMolecularMass = meanMolecularMass + m(j) * nAll(i,j) / ntotAll(i)
@@ -148,7 +148,7 @@ contains
     end do
 
     ! Average across all cells
-    meanMolecularMass = meanMolecularMass / cellsNumber
+    meanMolecularMass = meanMolecularMass / cellsNumber  !Trieu commented
     
     !call the solver (DVODE_f90)
     !CALL DVODE_F90(fex, &
